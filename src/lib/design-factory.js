bf1d6dc9d76d34a8d71df2a4b0bf6a872567614f
Eric Martinez <emartinez@usgs.gov>
2016-09-08 08:41:20 -0600

    Set up basic calculation chain structure. Updates tests.

--- src/lib/design-factory.js
+++ src/lib/design-factory.js
@@ -20,6 +20,7 @@ var DesignFactory = function (options) {
   _initialize = function (options) {
     options = extend(true, {}, _DEFAULTS, options);
 
+    _this.metadataFactory = options.metadataFactory;
     _this.probabilisticHazardFactory = options.probabilisticHazardFactory;
     _this.deterministicHazardFactory = options.deterministicHazardFactory;
     _this.riskTargetingFactory = options.riskTargetingFactory;
@@ -37,6 +38,63 @@ var DesignFactory = function (options) {
     _initialize = null;
     _this = null;
   };
+
+
+  _this.computeBasicDesign = function (/*data*/) {
+    return Promise.resolve({});
+  };
+
+  _this.computeFinalDesign = function (/*data*/) {
+    return Promise.resolve({});
+  };
+
+  _this.formatResult = function (/*result*/) {
+    return Promise.resolve({});
+  };
+
+  _this.getDesignData = function (inputs) {
+    var result;
+
+    result = {
+      metadata: null,
+      probabilistic: null,
+      deterministic: null,
+      riskCoefficients: null,
+      siteAmplification: null,
+      basicDesign: null,
+      finalDesign: null
+    };
+
+    return Promise.all([
+      _this.metadataFactory.getMetadata(inputs),
+      _this.probabilisticHazardFactory.getProbabilisticData(inputs),
+      _this.deterministicHazardFactory.getDeterministicData(inputs),
+      _this.riskTargetingFactory.getRiskCoefficients(inputs)
+    ]).then((promiseResults) => {
+      // => [metadata, probabilistic, deterministic, riskCoefficients]
+      result.metadata = promiseResults[0];
+      result.probabilistic = promiseResults[1];
+      result.deterministic = promiseResults[2];
+      result.riskCoefficients = promiseResults[3];
+
+      return _this.computeBasicDesign(result);
+    }).then((basicDesign) => {
+      result.basicDesign = basicDesign;
+
+      return _this.siteAmplificationFactory.getAmplificationData(
+          extend(true, {}, inputs, basicDesign));
+    }).then((siteAmplification) => {
+      result.siteAmplification = siteAmplification;
+
+      return _this.computeFinalDesign(result);
+    }).then((finalDesign) => {
+      result.finalDesign = finalDesign;
+
+      return _this.formatResult(result);
+    });
+  };
+
+
 
 
   _initialize(options);
--- test/design-factory.spec.js
+++ test/design-factory.spec.js
@@ -6,6 +6,16 @@ var DesignFactory = require('../src/lib/design-factory.js'),
     expect = require('chai').expect;
 
 
+var _DUMMY_FACTORY;
+
+_DUMMY_FACTORY = {
+  getMetadata: () => { return Promise.resolve({}); },
+  getProbabilisticData: () => { return Promise.resolve({}); },
+  getDeterministicData: () => { return Promise.resolve({}); },
+  getRiskCoefficients: () => { return Promise.resolve({}); }
+};
+
+
 describe('DesignFactory', () => {
   describe('constructor', () => {
     it('is defined', () => {
@@ -23,6 +33,60 @@ describe('DesignFactory', () => {
         factory = DesignFactory();
         factory.destroy();
       }).to.not.throw(Error);
+    });
+  });
+
+  describe('computeBasicDesign', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = DesignFactory();
+
+      expect(factory.computeBasicDesign()).to.be.instanceof(Promise);
+
+      factory.destroy();
+    });
+  });
+
+  describe('computeFinalDesign', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = DesignFactory();
+
+      expect(factory.computeFinalDesign()).to.be.instanceof(Promise);
+
+      factory.destroy();
+    });
+  });
+
+  describe('formatResult', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = DesignFactory();
+
+      expect(factory.formatResult()).to.be.instanceof(Promise);
+
+      factory.destroy();
+    });
+  });
+
+  describe('getDesignData', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = DesignFactory({
+        metadataFactory: _DUMMY_FACTORY,
+        probabilisticHazardFactory: _DUMMY_FACTORY,
+        deterministicHazardFactory: _DUMMY_FACTORY,
+        riskTargetingFactory: _DUMMY_FACTORY,
+        siteAmplificationFactory: _DUMMY_FACTORY
+      });
+
+      expect(factory.getDesignData()).to.be.instanceof(Promise);
+
+      factory.destroy();
     });
   });
 });
