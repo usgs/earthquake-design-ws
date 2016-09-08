99048081112947a33657a6816d999a02d101342b
ehunter-usgs <ehunter@usgs.gov>
2016-09-08 13:13:10 -0600

    Organize tests. Add test for getLegacyData

--- test/legacy-factory.spec.js
+++ test/legacy-factory.spec.js
@@ -3,49 +3,8 @@
 
 
 var expect = require('chai').expect,
-    LegacyFactory = require('../src/lib/legacy-factory');
-
-// Example calculation, with results for a site near Los Angeles
-var calculate = {
-  'input': {
-    'title': 'Los Angeles, CA',
-    'latitude': 34,
-    'longitude': -118,
-    'design_code': 1,
-    'risk_category': 1,
-    'site_class': 4
-  },
-  'output': {
-    'data': [
-      {
-        'latitude': 34,
-        'longitude': -118,
-        'mapped_ss': 1.91449,
-        'mapped_s1': 0.571707,
-        'mapped_pga': 0.819108,
-        'crs': 0.89639,
-        'cr1': 0.89975,
-        'geomean_ssd': 1.2282925,
-        'geomean_s1d': 0.4117471,
-        'geomean_pgad': 0.5670918
-      }
-    ],
-    'metadata': {
-      'region_name': 'Conterminous US',
-      'region_id': 6,
-      'max_direction_ss': 1.1,
-      'max_direction_s1': 1.3,
-      'percentile_ss': 1.8,
-      'percentile_s1': 1.8,
-      'percentile_pga': 1.8,
-      'deterministic_floor_ss': 1.5,
-      'deterministic_floor_s1': 0.6,
-      'deterministic_floor_pga': 0.5,
-      'grid_spacing': 0.05
-    },
-    'tl': null
-  }
-};
+    LegacyFactory = require('../src/lib/legacy-factory'),
+    sinon = require('sinon');
 
 var EPSILION = 0.00001;
 
@@ -109,34 +68,43 @@ describe('LegacyFactory test suite', () => {
     });
   });
 
-  describe('urlEncode', () => {
-    it('builds a url with api parameters', () => {
-      var factory,
-          inputs,
-          params,
-          url;
+  describe('getLegacyData', () => {
+    it('calls all helper methods', (done) => {
+      var cleanseStub,
+          interpolateStub,
+          response,
+          result,
+          requestStub;
 
-      factory = LegacyFactory();
-      inputs = {
-        design_code: 0,
-        site_class: 1,
-        risk_category: 2,
-        longitude: 3,
-        latitude: 4,
-        title: 5,
-      };
-      url = factory.urlEncode(inputs);
-      params = url.split('/');
+      response = {'data': 'test'};
 
-      expect(params[0]).to.equal('0');
-      expect(params[1]).to.equal('1');
-      expect(params[2]).to.equal('2');
-      expect(params[3]).to.equal('3');
-      expect(params[4]).to.equal('4');
-      expect(params[5]).to.equal('5');
+      cleanseStub = sinon.stub(legacyFactory, 'cleanseInputs', () => {
+        return {};
+      });
+
+      requestStub = sinon.stub(legacyFactory, 'makeRequest', () => {
+        return new Promise((resolve) => {
+          resolve(response);
+        });
+      });
+
+      interpolateStub = sinon.stub(legacyFactory, 'interpolate', () => {
+        return {};
+      });
+
+      result = legacyFactory.getLegacyData();
+
+      result.then(function () {
+        expect(cleanseStub.callCount).to.equal(1);
+        expect(requestStub.callCount).to.equal(1);
+        expect(interpolateStub.callCount).to.equal(1);
+        expect(interpolateStub.calledWith(response)).to.be.true;
+        done();
+      }).catch(function (err) {
+        done(err);
+      });
     });
   });
-
 
   describe('interpolates correctly', () => {
 
@@ -160,9 +128,52 @@ describe('LegacyFactory test suite', () => {
     });
 
     it('interpolates one point correctly', () => {
-      var interpolate;
+      var data,
+          interpolate;
 
-      interpolate = legacyFactory.interpolate(calculate);
+      // Sample data, with results for a site near Los Angeles
+      data = {
+        'input': {
+          'title': 'Los Angeles, CA',
+          'latitude': 34,
+          'longitude': -118,
+          'design_code': 1,
+          'risk_category': 1,
+          'site_class': 4
+        },
+        'output': {
+          'data': [
+            {
+              'latitude': 34,
+              'longitude': -118,
+              'mapped_ss': 1.91449,
+              'mapped_s1': 0.571707,
+              'mapped_pga': 0.819108,
+              'crs': 0.89639,
+              'cr1': 0.89975,
+              'geomean_ssd': 1.2282925,
+              'geomean_s1d': 0.4117471,
+              'geomean_pgad': 0.5670918
+            }
+          ],
+          'metadata': {
+            'region_name': 'Conterminous US',
+            'region_id': 6,
+            'max_direction_ss': 1.1,
+            'max_direction_s1': 1.3,
+            'percentile_ss': 1.8,
+            'percentile_s1': 1.8,
+            'percentile_pga': 1.8,
+            'deterministic_floor_ss': 1.5,
+            'deterministic_floor_s1': 0.6,
+            'deterministic_floor_pga': 0.5,
+            'grid_spacing': 0.05
+          },
+          'tl': null
+        }
+      };
+
+      interpolate = legacyFactory.interpolate(data);
 
       expect(JSON.stringify(interpolate)).to.equal(
           JSON.stringify({
@@ -264,4 +275,34 @@ describe('LegacyFactory test suite', () => {
           EPSILION);
     });
   });
+
+
+  describe('urlEncode', () => {
+    it('builds a url with api parameters', () => {
+      var factory,
+          inputs,
+          params,
+          url;
+
+      factory = LegacyFactory();
+      inputs = {
+        design_code: 0,
+        site_class: 1,
+        risk_category: 2,
+        longitude: 3,
+        latitude: 4,
+        title: 5,
+      };
+      url = factory.urlEncode(inputs);
+      params = url.split('/');
+
+      expect(params[0]).to.equal('0');
+      expect(params[1]).to.equal('1');
+      expect(params[2]).to.equal('2');
+      expect(params[3]).to.equal('3');
+      expect(params[4]).to.equal('4');
+      expect(params[5]).to.equal('5');
+    });
+  });
+
 });
