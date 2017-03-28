03e533785a1335d782b235478c361dc73b513b45
Jeremy Fee <jmfee@usgs.gov>
2017-03-28 13:26:10 -0600

    Add tests for UHTHazardCurveFactory getHazardCurveUrl, makeRequest, and parseHazardCurves methods

--- test/uht-hazard-curve-factory.spec.js
+++ test/uht-hazard-curve-factory.spec.js
@@ -3,6 +3,7 @@
 
 
 var expect = require('chai').expect,
+    express = require('express'),
     UHTHazardCurveFactory = require('../src/lib/uht-hazard-curve-factory');
 
 
@@ -131,6 +132,128 @@ describe('UHTHazardCurveFactory', () => {
           longitude: -118.1
         }
       ]);
+    });
+  });
+
+  describe('getHazardCurveUrl', () => {
+    var replacements;
+
+    replacements = {
+      hazardEdition: 'hazardEdition',
+      hazardRegion: 'hazardRegion',
+      latitude: 'latitude',
+      longitude: 'longitude',
+      imt: 'imt',
+      vs30: 'vs30'
+    };
+
+
+    it('replaces {edition} with options.hazardEdition', () => {
+      factory.url = '{edition}';
+      expect(factory.getHazardCurveUrl(replacements)).to.equal('hazardEdition');
+    });
+
+    it('replaces {region} with options.hazardRegion', () => {
+      factory.url = '{region}';
+      expect(factory.getHazardCurveUrl(replacements)).to.equal('hazardRegion');
+    });
+
+    it('replaces {latitude} with options.latitude', () => {
+      factory.url = '{latitude}';
+      expect(factory.getHazardCurveUrl(replacements)).to.equal('latitude');
+    });
+
+    it('replaces {longitude} with options.longitude', () => {
+      factory.url = '{longitude}';
+      expect(factory.getHazardCurveUrl(replacements)).to.equal('longitude');
+    });
+
+    it('replaces {imt} with "any"', () => {
+      factory.url = '{imt}';
+      expect(factory.getHazardCurveUrl(replacements)).to.equal('any');
+    });
+
+    it('replaces {vs30} with "760"', () => {
+      factory.url = '{vs30}';
+      expect(factory.getHazardCurveUrl(replacements)).to.equal('760');
+    });
+  });
+
+  describe('makeRequest', () => {
+    var testApp,
+        testPort,
+        testServer;
+
+    afterEach(() => {
+      testServer.close();
+    });
+
+    beforeEach((done) => {
+      testPort = 7999;
+      testApp = express();
+      testApp.use('', express.static('etc'));
+
+      testServer = testApp.listen(testPort, () => { done(); });
+    });
+
+    it('returns the expected response', (done) => {
+      var factory,
+          result;
+
+      factory = UHTHazardCurveFactory();
+
+      result = factory.makeRequest({
+        url: 'http://localhost:' + testPort + '/makeRequest.json'
+      });
+
+      result.then((data) => {
+        expect(data).to.deep.equal({'test':'working'});
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        done(err);
+      });
+    });
+  });
+
+  describe('parseHazardCurves', () => {
+    it('parses', () => {
+      var uhtResponse,
+          parsed;
+
+      uhtResponse = require('../etc/hazws-staticcurve-response.json');
+      parsed = factory.parseHazardCurves(uhtResponse);
+
+      expect(parsed.length).to.equal(7);
+      expect(parsed[0]).to.deep.equal({
+        hazardEdition: 'E2008R2',
+        hazardRegion: 'COUS0P05',
+        latitude: 34,
+        longitude: -118,
+        spectralPeriod: 'PGA',
+        vs30: '760',
+        data: [
+          [0.005, 0.4679],
+          [0.007, 0.3925],
+          [0.0098, 0.3172],
+          [0.0137, 0.248],
+          [0.0192, 0.188],
+          [0.0269, 0.139],
+          [0.0376, 0.1006],
+          [0.0527, 0.07031],
+          [0.0738, 0.04694],
+          [0.103, 0.02956],
+          [0.145, 0.0171],
+          [0.203, 0.00937],
+          [0.284, 0.004966],
+          [0.397, 0.002603],
+          [0.556, 0.001316],
+          [0.778, 0.0006023],
+          [1.09, 0.0002285],
+          [1.52, 0.00006808],
+          [2.13, 0.00001371]
+        ]
+      });
     });
   });
 });
