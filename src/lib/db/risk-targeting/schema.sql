4607cb0c405cd42bb70f5a7770162065f7ff7cd4
jdbrown-usgs <jdbrown@usgs.gov>
2017-04-11 14:13:05 -0600

    schema for rist coefficient

---
+++ src/lib/db/risk-targeting/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE region (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  grid_spacing NUMERIC NOT NULL,
+  max_latitude NUMERIC NOT NULL,
+  max_longitude NUMERIC NOT NULL,
+  min_latitude NUMERIC NOT NULL,
+  min_longitude NUMERIC NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE data (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  region_id INTEGER NOT NULL REFERENCES region(id),
+  latitude NUMERIC NOT NULL,
+  longitude NUMERIC NOT NULL,
+  mapped_cr1 NUMERIC NOT NULL,
+  mapped_crs NUMERIC NOT NULL
+);
+
+CREATE TABLE document (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  region_id INTEGER NOT NULL REFERENCES region(id),
+  interpolation_method VARCHAR(255) NOT NULL,
+  model_version VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
