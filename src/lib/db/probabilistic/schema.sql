173cc4b12916762ede4ac190dbc21010387e3f4a
jdbrown-usgs <jdbrown@usgs.gov>
2017-04-11 15:53:09 -0600

    set up schema

---
+++ src/lib/db/probabilistic/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE region (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  grid_spacing NUMERIC NOT NULL,
+  max_latitude NUMERIC NOT NULL,
+  max_longitude NUMERIC NOT NULL,
+  min_latitude NUMERIC NOT NULL,
+  min_longitude NUMERIC NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE data (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  region_id INTEGER NOT NULL REFERENCES region(id),
+  latitude NUMERIC NOT NULL,
+  longitude NUMERIC NOT NULL,
+  mapped_pga NUMERIC NOT NULL,
+  mapped_s1 NUMERIC NOT NULL,
+  mapped_ss NUMERIC NOT NULL
+);
+
+CREATE TABLE document (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  region_id INTEGER NOT NULL REFERENCES region(id),
+  interpolation_method VARCHAR(255) NOT NULL,
+  model_version VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
