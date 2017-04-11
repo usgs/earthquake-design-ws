725059cf084c0e53b73c8c6268055d6269533fe0
emartinez-usgs <emartinez@usgs.gov>
2017-04-11 09:13:18 -0600

    Added probabilistic handler and associated tests.

---
+++ src/lib/probabilistic-handler.js
@@ -0,0 +1,151 @@
+'use strict';
+
+
+var ProbabilisticFactory = require('./probabilistic-factory'),
+    extend = require('extend'),
+    Pool = require('./db/pool');
+
+
+var _DEFAULTS;
+
+_DEFAULTS = {
+  DB_DATABASE: 'postgres',
+  DB_HOST: 'localhost',
+  DB_PASSWORD: null,
+  DB_PORT: 5432,
+  DB_SCHEMA: 'probabilistic',
+  DB_USER: null
+};
+
+
+var ProbabilisticHandler = function (options) {
+  var _this,
+      _initialize;
+
+
+  _this = {};
+
+  _initialize = function (options) {
+    options = extend(true, {}, _DEFAULTS, options);
+    if (options.hasOwnProperty('DB_SCHEMA_PROBABILISTIC')) {
+      options.DB_SCHEMA = options.DB_SCHEMA_PROBABILISTIC;
+    }
+
+
+    if (options.factory) {
+      _this.factory = options.factory;
+    } else {
+      _this.destroyFactory = true;
+      _this.factory = ProbabilisticFactory({
+        db: _this.createDbPool(options)
+      });
+    }
+  };
+
+
+  _this.checkParams = function (params) {
+    var buf,
+        err,
+        latitude,
+        longitude,
+        referenceDocument;
+
+    buf = [];
+
+    latitude = params.latitude;
+    longitude = params.longitude;
+    referenceDocument = params.referenceDocument;
+
+    if (typeof latitude === 'undefined' || latitude === null) {
+      buf.push('latitude');
+    }
+
+    if (typeof longitude === 'undefined' || longitude === null) {
+      buf.push('longitude');
+    }
+
+    if (typeof referenceDocument === 'undefined' ||
+        referenceDocument === null) {
+      buf.push('referenceDocument');
+    }
+
+    if (buf.length > 0) {
+      err = new Error('Missing required parameter' +
+          (buf.length > 1 ? 's' : '') + ': ' + buf.join(', '));
+      err.status = 400;
+      return Promise.reject(err);
+    }
+
+    return Promise.resolve(params);
+  };
+
+  _this.createDbPool = function (options) {
+    options = options || _DEFAULTS;
+
+    if (!_this.db) {
+      _this.destroyDb = true;
+      _this.db = Pool(options);
+    }
+
+    return _this.db;
+  };
+
+  _this.destroy = function () {
+    if (_this === null) {
+      return;
+    }
+
+    if (_this.destroyFactory && _this.factory) {
+      _this.factory.destroy();
+      _this.factory = null;
+    }
+
+    if (_this.destroyDb) {
+      _this.db.destroy(); // Technically async, but what would we do anyway?
+    }
+
+    _initialize = null;
+    _this = null;
+  };
+
+  _this.formatResult = function (result) {
+    return new Promise((resolve, reject) => {
+      var formatted;
+
+      try {
+        formatted = {
+          data: {
+            pga: result.data.mapped_pga,
+            s1: result.data.mapped_s1,
+            ss: result.data.mapped_ss
+          },
+          metadata: {
+            region_name: result.metadata.region.name,
+            model_version: result.metadata.document.model_version,
+            interpolation_method: result.metadata.document.interpolation_method
+          }
+        };
+
+        return resolve(formatted);
+      } catch (e) {
+        return reject(e);
+      }
+    });
+  };
+
+  _this.get = function (params) {
+    return _this.checkParams(params).then((params) => {
+      return _this.factory.getProbabilisticData(params);
+    }).then((result) => {
+      return _this.formatResult(result);
+    });
+  };
+
+
+  _initialize(options);
+  options = null;
+  return _this;
+};
+
+
+module.exports = ProbabilisticHandler;
---
+++ test/probabilistic-handler.spec.js
@@ -0,0 +1,197 @@
+/* global afterEach, beforeEach, describe, it */
+'use strict';
+
+var ProbabilisticHandler = require('../src/lib/probabilistic-handler'),
+    expect = require('chai').expect,
+    sinon = require('sinon');
+
+
+var _FACTORY,
+    _INPUT,
+    _RESULT;
+
+
+_INPUT = {
+  latitude: 34.0,
+  longitude: -118.0,
+  referenceDocument: 'ASCE41-13'
+};
+
+_FACTORY = {
+  destroy: () => {
+    // Nothing to do here
+  },
+  getProbabilisticData: () => {
+    return Promise.resolve(_RESULT);
+  }
+};
+
+_RESULT = {
+  'data': {
+    'id': 9859572,
+    'region_id': 4,
+    'latitude': 34,
+    'longitude': -118,
+    'pga': 0.89359,
+    's1': 0.67503,
+    'ss': 2.2676
+  },
+  'metadata': {
+    'region': {
+      'id': 4,
+      'grid_spacing': 0.01,
+      'max_latitude': 50,
+      'max_longitude': -65,
+      'min_latitude': 24.6,
+      'min_longitude': -125,
+      'name': 'E2008R2_COUS0P01_Probabilistic'
+    },
+    'document': {
+      'id': 3,
+      'region_id': 4,
+      'interpolation_method': 'linear',
+      'model_version': 'v3.1.x',
+      'name': 'ASCE41-13'
+    }
+  }
+};
+
+
+describe('probabilistic-handler', () => {
+  describe('constructor', () => {
+    it('is defined', () => {
+      expect(typeof ProbabilisticHandler).to.equal('function');
+    });
+
+    it('can be instantiated', () => {
+      expect(ProbabilisticHandler).to.not.throw(Error);
+    });
+
+    it('can be destroyed', () => {
+      var handler;
+
+      handler = ProbabilisticHandler();
+      expect(handler.destroy).to.not.throw(Error);
+    });
+  });
+
+  describe('checkParams', () => {
+    var handler;
+
+    afterEach(() => {
+      handler.destroy();
+    });
+
+    beforeEach(() => {
+      handler = ProbabilisticHandler({factory: _FACTORY});
+    });
+
+    it('returns error if parameters are missing', (done) => {
+      handler.checkParams({}).then(() => {
+        return new Error('checkParams passed erroneously');
+      }).catch((err) => {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.status).to.equal(400);
+      }).then(done);
+    });
+  });
+
+  describe('createDbPool', () => {
+    it('sets _this.db to a Pool', () => {
+      var handler;
+
+      handler = ProbabilisticHandler({factory: _FACTORY});
+
+      expect(handler.db).to.be.undefined;
+      handler.createDbPool();
+
+      // Do a duck-type check on the db
+      expect(typeof handler.db.destroy).to.equal('function');
+      expect(typeof handler.db.query).to.equal('function');
+
+      handler.destroy();
+      handler = null;
+    });
+  });
+
+  describe('formatResult', () => {
+    var handler;
+
+    afterEach(() => {
+      handler.destroy();
+      handler = null;
+    });
+
+    beforeEach(() => {
+      handler = ProbabilisticHandler({factory: _FACTORY});
+    });
+
+
+    it('resolves with object with appropriate structure', (done) => {
+      var formatted;
+
+      formatted = handler.formatResult(_RESULT);
+      expect(formatted).to.be.instanceof(Promise);
+
+      formatted.then((result) => {
+        var data,
+            metadata;
+
+        expect(result.hasOwnProperty('data')).to.be.true;
+        expect(result.hasOwnProperty('metadata')).to.be.true;
+
+        data = result.data;
+        metadata = result.metadata;
+
+        expect(data.hasOwnProperty('pga')).to.be.true;
+        expect(data.hasOwnProperty('s1')).to.be.true;
+        expect(data.hasOwnProperty('ss')).to.be.true;
+
+        expect(metadata.hasOwnProperty('region_name')).to.be.true;
+        expect(metadata.hasOwnProperty('model_version')).to.be.true;
+        expect(metadata.hasOwnProperty('interpolation_method')).to.be.true;
+      }).catch((err) => {
+        return err;
+      }).then(done);
+    });
+
+    it('rejects if input object is invalid', (done) => {
+      handler.formatResult({}).then(() => {
+        done(new Error('Result was invalid and should have rejected, ' +
+            'but instead resolved.'));
+      }).catch((/*err*/) => {
+        done();
+      });
+    });
+  });
+
+  describe('get', () => {
+    it('checks params and defers to factory', (done) => {
+      var handler,
+          spy,
+          stub;
+
+      handler = ProbabilisticHandler({factory: _FACTORY});
+
+      stub = sinon.stub(handler, 'checkParams', () => {
+        return Promise.resolve(_INPUT);
+      });
+      spy = sinon.spy(handler.factory, 'getProbabilisticData');
+
+      handler.get(_INPUT).then(() => {
+        expect(stub.callCount).to.equal(1);
+        expect(spy.callCount).to.equal(1);
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        stub.restore();
+        spy.restore();
+        handler.destroy();
+        handler = null;
+        done(err);
+      });
+
+      handler.checkParams.restore();
+    });
+  });
+});
