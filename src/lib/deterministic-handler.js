aeb71227d4f780e3e59dcb3fc1ea90cd875a93dd
emartinez-usgs <emartinez@usgs.gov>
2017-04-12 14:59:22 -0600

    Moved tests out of specific handlers into abstract handler.

--- src/lib/deterministic-handler.js
+++ src/lib/deterministic-handler.js
@@ -1,147 +1,32 @@
 'use strict';
 
 
-var DeterministicFactory = require('./gridded-data-factory'),
-    extend = require('extend'),
-    Pool = require('./db/pool');
+var extend = require('extend'),
+    GriddedDataHandler = require('./gridded-data-handler');
 
 
 var _DEFAULTS;
 
 _DEFAULTS = {
-  DB_DATABASE: 'postgres',
-  DB_HOST: 'localhost',
-  DB_PASSWORD: null,
-  DB_PORT: 5432,
-  DB_SCHEMA: 'deterministic',
-  DB_USER: null
+  DB_SCHEMA: 'deterministic'
 };
 
 
 var DeterministicHandler = function (options) {
-  var _this,
-      _initialize;
+  var _this;
 
 
-  _this = {};
-
-  _initialize = function (options) {
-    options = extend(true, {}, _DEFAULTS, options);
-    if (options.hasOwnProperty('DB_SCHEMA_DETERMINISTIC')) {
-      options.DB_SCHEMA = options.DB_SCHEMA_DETERMINISTIC;
-    }
-
-    if (options.factory) {
-      _this.factory = options.factory;
-    } else {
-      _this.destroyFactory = true;
-      _this.factory = DeterministicFactory({
-        db: _this.createDbPool(options)
-      });
-    }
-  };
+  options = extend(true, {}, _DEFAULTS, options);
+  if (options.hasOwnProperty('DB_SCHEMA_DETERMINISTIC')) {
+    options.DB_SCHEMA = options.DB_SCHEMA_DETERMINISTIC;
+  }
+  _this = GriddedDataHandler(options);
 
 
-  _this.checkParams = function (params) {
-    var buf,
-        err,
-        latitude,
-        longitude,
-        referenceDocument;
-
-    buf = [];
-
-    latitude = params.latitude;
-    longitude = params.longitude;
-    referenceDocument = params.referenceDocument;
-
-    if (typeof latitude === 'undefined' || latitude === null) {
-      buf.push('latitude');
-    }
-
-    if (typeof longitude === 'undefined' || longitude === null) {
-      buf.push('longitude');
-    }
-
-    if (typeof referenceDocument === 'undefined' ||
-        referenceDocument === null) {
-      buf.push('referenceDocument');
-    }
-
-    if (buf.length > 0) {
-      err = new Error('Missing required parameter' +
-          (buf.length > 1 ? 's' : '') + ': ' + buf.join(', '));
-      err.status = 400;
-      return Promise.reject(err);
-    }
-
-    return Promise.resolve(params);
-  };
-
-  _this.createDbPool = function (options) {
-    options = options || _DEFAULTS;
-
-    if (!_this.db) {
-      _this.destroyDb = true;
-      _this.db = Pool(extend(options));
-    }
-
-    return _this.db;
-  };
-
-  _this.destroy = function () {
-    if (_this === null) {
-      return;
-    }
-
-    if (_this.destroyFactory && _this.factory) {
-      _this.factory.destroy();
-      _this.factory = null;
-    }
-
-    if (_this.destroyDb) {
-      _this.db.destroy(); // Technically async, but what would we do anyway?
-    }
-
-    _initialize = null;
-    _this = null;
-  };
-
-  _this.formatResult = function (result) {
-    return new Promise((resolve, reject) => {
-      var formatted;
-
-      try {
-        formatted = {
-          data: {
-            pgad: result.data.pgad,
-            s1d: result.data.s1d,
-            ssd: result.data.ssd
-          },
-          metadata: {
-            interpolation_method: result.metadata.document.interpolation_method,
-            model_version: result.metadata.document.model_version,
-            region_name: result.metadata.region.name
-          }
-        };
-
-        return resolve(formatted);
-      } catch (e) {
-        return reject(e);
-      }
-    });
-  };
-
-  _this.get = function (params) {
-    return _this.checkParams(params).then((params) => {
-      return _this.factory.get(params);
-    }).then((result) => {
-      return _this.formatResult(result);
-    });
-  };
+  // Override any API or Helper methods from GriddedDataHander as needed.
+  // ...
 
 
-  _initialize(options);
   options = null;
   return _this;
 };
