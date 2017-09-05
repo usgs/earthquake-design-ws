cd58a52f6512ef8ce788b17df1d9375104ad1d3b
Eric Martinez <emartinez@usgs.gov>
2017-09-05 13:58:34 -0600

    Updated tests for metadata factory.

--- test/metadata-factory.spec.js
+++ test/metadata-factory.spec.js
@@ -2,14 +2,14 @@
 'use strict';
 
 
-var expect = require('chai').expect,
+const expect = require('chai').expect,
     MetadataFactory = require('../src/lib/metadata-factory'),
     NumberUtils = require('../src/lib/util/number-utils').instance,
     sinon = require('sinon');
 
 
 describe('MetadataFactory', () => {
-  var factory;
+  let factory;
 
   beforeEach(() => {
     factory = MetadataFactory();
@@ -31,7 +31,7 @@ describe('MetadataFactory', () => {
 
     it('can be destroyed', () => {
       expect(() => {
-        var factory;
+        let factory;
 
         factory = MetadataFactory();
         factory.destroy();
@@ -47,7 +47,7 @@ describe('MetadataFactory', () => {
 
     it('throws an error when params are not passed', (done) => {
       factory.getMetadata().then((/*result*/) => {
-        var error;
+        let error;
 
         error = new Error('Method resolved but should have rejected!');
         error.assertionFailed = true; // Flag to distinguish this error
@@ -64,7 +64,7 @@ describe('MetadataFactory', () => {
     });
 
     it('calls each factory method', (done) => {
-      var result;
+      let result;
 
       sinon.spy(factory, 'getData');
       sinon.spy(factory, 'getRegion');
@@ -96,12 +96,45 @@ describe('MetadataFactory', () => {
     it ('returns a promise', () => {
       expect(factory.getData()).to.be.an.instanceof(Promise);
     });
+
+    it('returns ASCE7-10 metadata', (done) => {
+      Promise.all([
+        factory.getData('ASCE7-10', 'COUS'),
+        factory.getData('ASCE7-10', 'HI')
+      ]).then((results) => {
+        let cous,
+            hi;
+
+        cous = results[0];
+        hi = results[1];
+
+        expect(cous.pgadPercentileFactor).to.equal(1.8);
+        expect(cous.pgadFloor).to.equal(0.5);
+        expect(cous.s1MaxDirFactor).to.equal(1.3);
+        expect(cous.s1dPercentileFactor).to.equal(1.8);
+        expect(cous.s1dFloor).to.equal(0.6);
+        expect(cous.ssMaxDirFactor).to.equal(1.1);
+        expect(cous.ssdPercentileFactor).to.equal(1.8);
+        expect(cous.ssdFloor).to.equal(1.5);
+
+        expect(hi.pgadPercentileFactor).to.equal(1.8);
+        expect(hi.pgadFloor).to.equal(0.5);
+        expect(hi.s1MaxDirFactor).to.equal(1.0);
+        expect(hi.s1dPercentileFactor).to.equal(1.8);
+        expect(hi.s1dFloor).to.equal(0.6);
+        expect(hi.ssMaxDirFactor).to.equal(1.0);
+        expect(hi.ssdPercentileFactor).to.equal(1.8);
+        expect(hi.ssdFloor).to.equal(1.5);
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        done(err);
+      });
+    });
+
     // returns correct region
     it('returns ASCE41-13 metadata', (done) => {
-      var result;
-
-      result = factory.getData('ASCE41-13', 'COUS');
-      result.then((results) => {
+      factory.getData('ASCE41-13', 'COUS').then((results) => {
         expect(results.curve_interpolation_method).to.equal(
             NumberUtils.INTERPOLATE_USING_LOG);
       }).catch((err) => {
@@ -112,10 +145,7 @@ describe('MetadataFactory', () => {
     });
 
     it('returns ASCE7-16 metadata', (done) => {
-      var result;
-
-      result = factory.getData('ASCE7-16', 'COUS');
-      result.then((results) => {
+      factory.getData('ASCE7-16', 'COUS').then((results) => {
         expect(results.pgadPercentileFactor).to.equal(1.8);
       }).catch((err) => {
         return err;
@@ -129,6 +159,7 @@ describe('MetadataFactory', () => {
     it ('returns a promise', () => {
       expect(factory.getRegion()).to.be.an.instanceof(Promise);
     });
+
     // returns correct data
     it('returns a region', (done) => {
       Promise.all([
