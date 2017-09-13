744604be25de87e9ec3bbd337fa8fd21db7bb3ec
jcaple <jcaple@usgs.gov>
2017-09-13 09:51:58 -0600

    Starting unit test work

--- test/asce7_16-factory.spec.js
+++ test/asce7_16-factory.spec.js
@@ -4,9 +4,36 @@
 
 const ASCE7_16Factory = require('../src/lib/asce7_16-factory'),
     expect = require('chai').expect;
+    //sinon = require('sinon');
 
+const _DUMMY_FACTORY = {
+  metadataFactory: {
+    getMetadata: () => { return Promise.resolve([]); }
+  },
+  probabilisticService: {
+    getData: () => { return Promise.resolve({}); }
+  },
+  deterministicService: {
+    getData: () => { return Promise.resolve({}); }
+  },
+  riskCoefficientService: {
+    getData: () => { return Promise.resolve({}); }
+  },
+  tSubLService: {
+    getData: () => { return Promise.resolve({response: {data: {}}}); }
+  },
+  siteAmplificationFactory: {
+    getSiteAmplificationData: () => { return Promise.resolve([]); }
+  },
+  designCategoryFactory: {
+    getDesignCategory: () => { return Promise.resolve([]); }
+  },
+  spectraFactory: {
+    getSpectrum: () => { return Promise.resolve([]); }
+  }
+};
 
-describe('asce7_16-factory', () => {
+describe.only('asce7_16-factory', () => {
   describe('constructor', () => {
     it('is defined', () => {
       expect(typeof ASCE7_16Factory).to.not.equal('undefined');
@@ -27,6 +54,27 @@ describe('asce7_16-factory', () => {
 
       const factory = ASCE7_16Factory();
       expect(factory.referenceDocument).to.equal('ASCE7-16');
+    });
+  });
+
+  describe('get', () => {
+    it('returns a promise and calls functions as intended', () => {
+      var result,
+          factory;
+
+      factory = ASCE7_16Factory(_DUMMY_FACTORY);
+
+      result = factory.get({
+        latitude: 0,
+        longitude: 1,
+        riskCategory: 2,
+        siteClass: 'C',
+        title: 'Test'
+      });
+
+      expect(result).to.be.instanceof(Promise);
+      
+      console.log(JSON.stringify(result, null, 2));
     });
   });
 });
