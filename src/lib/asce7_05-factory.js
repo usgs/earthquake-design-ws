7e0175309c1576469eadf5c7665de557c77652aa
ehunter-usgs <ehunter@usgs.gov>
2017-09-14 14:46:57 -0600

    Create ASCE7-05Factory

---
+++ src/lib/asce7_05-factory.js
@@ -0,0 +1,276 @@
+'use strict';
+
+
+const ASCE7_10Factory = require('./asce7_10-factory'),
+    extend = require('extend'),
+    NumberUtils = require('./util/number-utils').instance;
+
+
+const _DEFAULTS = {
+  referenceDocument: 'ASCE7-05'
+};
+
+
+/**
+ * Class: ASCE7_05Factory
+ *
+ * @param options Object
+ *     Configuration options for this instance.
+ */
+const ASCE7_05Factory = function (options) {
+  let _this;
+
+
+  options = extend({}, _DEFAULTS, options);
+  _this = ASCE7_10Factory(options);
+
+
+  /**
+   * Computes Ss and S1 values from initial result `data` fetched from
+   * the metadata- and probabilistic- factories.
+   *
+   * @param data {Array}
+   *     An array containing 1,2, or 4 gridded data point results ...
+   * @param data.metadata {Object}
+   *     An object containing metadata for the calculation
+   * @param data.probabilistic {Object}
+   *     An object containing probabilistic hazard data for the caluclation
+   *
+   * @return {Promise}
+   *     A promise that will resolve with an object containing "ss" and "s1"
+   *     keys with corresponding data.
+   */
+  _this.computeBasicDesign = function (data) {
+    // return Promise.resolve({});
+    return new Promise((resolve, reject) => {
+      let basicDesign,
+          inputs,
+          metadata,
+          probabilistic;
+
+      basicDesign = [];
+      inputs = data.inputs;
+
+      try {
+        metadata = data.metadata;
+        probabilistic = data.probabilistic.map((item) => {
+          return item.response.data;
+        });
+
+        for (let i = 0, len = probabilistic.length; i < len; i++) {
+          let probabilisticItem,
+              result;
+
+          probabilisticItem = probabilistic[i];
+          result = {
+            latitude: parseFloat(data.probabilistic[i].request.parameters.latitude),
+            longitude: parseFloat(data.probabilistic[i].request.parameters.longitude)
+          };
+
+          // Compute Ss
+          result.ssuh = _this.computeUniformHazard(probabilisticItem.ss,
+              metadata.ssMaxDirFactor);
+
+          // Compute S1
+          result.s1uh = _this.computeUniformHazard(probabilisticItem.s1,
+              metadata.s1MaxDirFactor);
+
+          basicDesign.push(result);
+        }
+
+        // interpolate result
+        basicDesign = NumberUtils.spatialInterpolate(basicDesign, inputs.latitude, inputs.longitude, inputs.spatial_interpolation_method);
+
+        basicDesign.ss = _this.computeGroundMotion(basicDesign.ssrt,
+            basicDesign.ssd);
+        basicDesign.s1 = _this.computeGroundMotion(basicDesign.s1rt,
+            basicDesign.s1d);
+
+        resolve(basicDesign);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
+
+  /**
+   * Computes the final ground motion values applying site amplification
+   * factors and design (2/3) weighting.
+   *
+   * @param data {Object}
+   *     An object containing `basicDesign` and `siteAmplification` information.
+   * @param data.basicDesign {Object}
+   *     An object containing `ss` and `s1`ground motion values
+   * @param data.siteAmplification {Object}
+   *     An object containing `fa` and `fv` ground motion values
+   *
+   * @return {Object}
+   *    An object containing the final design ground motion data, namely:
+   *    `sms`, `sm1`, `sds`, `sd1`
+   */
+  _this.computeFinalDesign = function (data) {
+    // return Promise.resolve({});
+    return new Promise((resolve, reject) => {
+      let basicDesign,
+          finalDesign,
+          siteAmplification;
+
+      finalDesign = {
+        sms: null,
+        sm1: null,
+        sds: null,
+        sd1: null
+      };
+
+      try {
+        basicDesign = data.basicDesign;
+        siteAmplification = data.siteAmplification;
+
+        finalDesign.sms = _this.computeSiteModifiedValue(basicDesign.ss,
+            siteAmplification.fa);
+        finalDesign.sm1 = _this.computeSiteModifiedValue(basicDesign.s1,
+            siteAmplification.fv);
+
+        finalDesign.sds = _this.computeDesignValue(finalDesign.sms);
+        finalDesign.sd1 = _this.computeDesignValue(finalDesign.sm1);
+
+        resolve(finalDesign);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
+
+  _this.formatResult = function (result) {
+    return new Promise((resolve, reject) => {
+      let basicDesign,
+          designCategory,
+          finalDesign,
+          siteAmplification,
+          spectra;
+
+      try {
+        basicDesign = result.basicDesign;
+        designCategory = result.designCategory;
+        finalDesign = result.finalDesign;
+        siteAmplification = result.siteAmplification;
+        spectra = result.spectra;
+
+
+        resolve({
+          data: {
+            ssrt: NumberUtils.round(basicDesign.ssrt,
+                _this.outputDecimals),
+            ssuh: NumberUtils.round(basicDesign.ssuh,
+                _this.outputDecimals),
+            ss: NumberUtils.round(basicDesign.ss,
+                _this.outputDecimals),
+            fa: NumberUtils.round(siteAmplification.fa, _this.outputDecimals),
+            fa_error: siteAmplification.fa_error,
+            sms: NumberUtils.round(finalDesign.sms, _this.outputDecimals),
+            sds: NumberUtils.round(finalDesign.sds, _this.outputDecimals),
+            sdcs: designCategory.sdcs,
+
+            s1rt: NumberUtils.round(basicDesign.s1rt,
+                _this.outputDecimals),
+            s1uh: NumberUtils.round(basicDesign.s1uh,
+                _this.outputDecimals),
+            s1: NumberUtils.round(basicDesign.s1,
+                _this.outputDecimals),
+            fv: NumberUtils.round(siteAmplification.fv, _this.outputDecimals),
+            fv_error: siteAmplification.fv_error,
+            sm1: NumberUtils.round(finalDesign.sm1, _this.outputDecimals),
+            sd1: NumberUtils.round(finalDesign.sd1, _this.outputDecimals),
+            sdc1: designCategory.sdc1,
+
+            sdc: designCategory.sdc,
+            't-sub-l': result.tSubL,
+
+            sdSpectrum: (siteAmplification.fa === null || siteAmplification.fv === null) ?
+                null :
+                NumberUtils.roundSpectrum(spectra.sdSpectrum, _this.outputDecimals),
+            smSpectrum: (siteAmplification.fa === null || siteAmplification.fv === null) ?
+                null :
+                NumberUtils.roundSpectrum(spectra.smSpectrum, _this.outputDecimals)
+          },
+
+          metadata: extend(true, {}, result.metadata)
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
+
+  _this.get = function (inputs) {
+    let result;
+
+    inputs = inputs || {};
+    inputs.referenceDocument = _this.referenceDocument;
+
+    result = {
+      basicDesign: null,
+      finalDesign: null,
+      metadata: null,
+      probabilistic: null,
+      siteAmplification: null,
+      tSubL: null
+    };
+
+    return Promise.all([
+      _this.probabilisticService.getData(inputs),
+    ]).then((promiseResults) => {
+      let probabilisticInputs;
+
+      probabilisticInputs = extend(
+          {gridSpacing: promiseResults[0].response.metadata.gridSpacing},
+          inputs
+        );
+
+      return Promise.all([
+        _this.makeMultipleRequests(
+            NumberUtils.getGridPoints(probabilisticInputs),
+            probabilisticInputs,
+            _this.probabilisticService
+          ),
+        _this.metadataFactory.getMetadata(inputs),
+        _this.tSubLService.getData(inputs)
+      ]);
+    }).then((promiseResults) => {
+      result.probabilistic = promiseResults[0];
+      result.metadata = promiseResults[1];
+      result.tSubL = promiseResults[2].response.data['t-sub-l'];
+      result.inputs = inputs;
+
+      return _this.computeBasicDesign(result);
+    }).then((basicDesign) => {
+      result.basicDesign = basicDesign;
+
+      return _this.siteAmplificationFactory.getSiteAmplificationData(
+          extend(true, {}, inputs, basicDesign));
+    }).then((siteAmplification) => {
+      result.siteAmplification = siteAmplification;
+
+      return _this.computeFinalDesign(result);
+    }).then((finalDesign) => {
+      result.finalDesign = finalDesign;
+
+      return Promise.all([
+        _this.designCategoryFactory.getDesignCategory(inputs.riskCategory,
+            result.basicDesign.s1, finalDesign.sds, finalDesign.sd1),
+        _this.computeSpectra(finalDesign)
+      ]);
+    }).then((promiseResults) => {
+      result.designCategory = promiseResults[0];
+      result.spectra = promiseResults[1];
+
+      return _this.formatResult(result);
+    });
+  };
+
+  options = null;
+  return _this;
+};
+
+
+module.exports = ASCE7_05Factory;
---
+++ test/asce7_05-factory.spec.js
@@ -0,0 +1,335 @@
+/* global describe, it */
+'use strict';
+
+
+var ASCE7_05Factory = require('../src/lib/asce7_05-factory.js'),
+    expect = require('chai').expect,
+    sinon = require('sinon');
+
+
+var _DUMMY_FACTORY;
+
+_DUMMY_FACTORY = {
+  metadataFactory: {
+    getMetadata: () => { return Promise.resolve([]); }
+  },
+  probabilisticService: {
+    getData: () => {
+      return Promise.resolve({
+        response: {
+          metadata: {
+            gridSpacing: null
+          }
+        }
+      });
+    }
+  },
+  tSubLService: {
+    getData: () => { return Promise.resolve({response: {data: {}}}); }
+  },
+  siteAmplificationFactory: {
+    getSiteAmplificationData: () => { return Promise.resolve([]); }
+  },
+  designCategoryFactory: {
+    getDesignCategory: () => { return Promise.resolve([]); }
+  },
+  spectraFactory: {
+    getHorizontalSpectrum: () => { return Promise.resolve([]); }
+  }
+};
+
+
+describe('ASCE7_05Factory', () => {
+  describe('constructor', () => {
+    it('is defined', () => {
+      expect(typeof ASCE7_05Factory).to.equal('function');
+    });
+
+    it('can be instantiated', () => {
+      expect(ASCE7_05Factory).to.not.throw(Error);
+    });
+
+    it('can be destroyed', () => {
+      expect(() => {
+        var factory;
+
+        factory = ASCE7_05Factory();
+        factory.destroy();
+      }).to.not.throw(Error);
+    });
+  });
+
+  describe('computeBasicDesign', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = ASCE7_05Factory();
+
+      expect(factory.computeBasicDesign()).to.be.instanceof(Promise);
+
+      factory.destroy();
+    });
+
+    it('rejects with an error when one occurs', function (done) {
+      var factory;
+
+      factory = ASCE7_05Factory();
+
+      factory.computeBasicDesign().then((/*obj*/) => {
+        // This should not execute because we expect a rejected promise
+        var err;
+
+        err = new Error('[computeBasicDesign] resolved when it should reject');
+        err.unitTestFailed = true;
+
+        throw err;
+      }).catch((err) => {
+        if (err.unitTestFailed) {
+          return err;
+        }
+      }).then((err) => {
+        done(err);
+      });
+    });
+
+    it('calls expected calculation methods', (done) => {
+      var factory;
+
+      factory = ASCE7_05Factory();
+
+      sinon.spy(factory, 'computeUniformHazard');
+      sinon.spy(factory, 'computeDeterministic');
+      sinon.spy(factory, 'computeGroundMotion');
+
+      factory.computeBasicDesign({
+        inputs: {
+          latitude: 35,
+          longitude: -118
+        },
+        metadata: {
+          ssMaxDirection: 0,
+          ssPercentile: 0,
+          ssdFloor: 0,
+          s1MaxDirection: 0,
+          s1Percentile: 0,
+          s1dFloor: 0,
+        },
+        probabilistic: [{
+          request: {
+            parameters: {
+              latitude: 35,
+              longitude: -118
+            }
+          },
+          response: {
+            data: {ss: 0, s1: 0, pga: 0}
+          }
+        }]
+      }).then(() => {
+        expect(factory.computeUniformHazard.callCount).to.equal(2);
+        expect(factory.computeGroundMotion.callCount).to.equal(2);
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        factory.computeUniformHazard.restore();
+        factory.computeGroundMotion.restore();
+
+        factory.destroy();
+
+        done(err);
+      });
+    });
+  });
+
+  describe('computeFinalDesign', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = ASCE7_05Factory();
+
+      expect(factory.computeFinalDesign()).to.be.instanceof(Promise);
+
+      factory.destroy();
+    });
+
+    it('calls expected calculation methods', (done) => {
+      var factory;
+
+      factory = ASCE7_05Factory();
+
+      sinon.spy(factory, 'computeSiteModifiedValue');
+      sinon.spy(factory, 'computeDesignValue');
+
+      factory.computeFinalDesign({
+        basicDesign: {ss: 0, s1: 0, pga: 0},
+        siteAmplification: {fa: 0, fv: 0, fpga: 0}
+      }).then(() => {
+        expect(factory.computeSiteModifiedValue.callCount).to.equal(2);
+        expect(factory.computeDesignValue.callCount).to.equal(2);
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        factory.computeSiteModifiedValue.restore();
+        factory.computeDesignValue.restore();
+
+        factory.destroy();
+
+        done(err);
+      });
+    });
+  });
+
+  describe('formatResult', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = ASCE7_05Factory();
+
+      expect(factory.formatResult()).to.be.instanceof(Promise);
+
+      factory.destroy();
+    });
+
+    it('resolves with expected data structure', (done) => {
+      var factory;
+
+      factory = ASCE7_05Factory();
+
+      factory.formatResult({
+        'basicDesign': {
+          'ss': null,
+          's1': null,
+          'pga': null,
+          'ssuh': null,
+          'ssrt': null,
+          'ssd': null,
+          's1uh': null,
+          's1rt': null,
+          's1d': null,
+          'pgad': null
+        },
+        'deterministic': {},
+        'finalDesign': {
+          'sms': null,
+          'sm1': null,
+          'pgam': null,
+          'sds': null,
+          'sd1': null
+        },
+        'metadata': {
+          'pgadPercentileFactor': null,
+          'pgadFloor': null,
+
+          's1MaxDirFactor': null,
+          's1dPercentileFactor': null,
+          's1dFloor': null,
+
+          'ssMaxDirFactor': null,
+          'ssdPercentileFactor': null,
+          'ssdFloor': null
+        },
+        'probabilistic': {},
+        'riskCoefficients': {
+          'response': {
+            'data': {}
+          }
+        },
+        'siteAmplification': {
+          'fa': null,
+          'fa_error': null,
+          'fv': null,
+          'fv_error': null
+        },
+        'designCategory': {
+          'sdcs': null,
+          'sdc1': null,
+          'sdc': null
+        },
+        'spectra': {
+          'smSpectrum': [],
+          'sdSpectrum': []
+        }
+      }).then((formatted) => {
+        [
+          'ssuh', 's1uh',
+          'ssrt', 's1rt',
+          'ss', 's1',
+          'sms', 'sm1',
+          'fa', 'fa_error', 'fv', 'fv_error',
+          'sds', 'sdcs', 'sd1', 'sdc1', 'sdc',
+          'smSpectrum', 'sdSpectrum'
+        ].forEach((key) => {
+          expect(formatted.data.hasOwnProperty(key)).to.equal(true);
+        });
+        [
+          'pgadPercentileFactor', 'pgadFloor',
+          's1MaxDirFactor', 's1dPercentileFactor', 's1dFloor',
+          'ssMaxDirFactor', 'ssdPercentileFactor', 'ssdFloor'
+        ].forEach((key) => {
+          expect(formatted.metadata.hasOwnProperty(key)).to.equal(true);
+        });
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        factory.destroy();
+        done(err);
+      });
+    });
+  });
+
+  describe('get', () => {
+    it('returns a promise', () => {
+      var factory;
+
+      factory = ASCE7_05Factory(_DUMMY_FACTORY);
+
+      expect(factory.get()).to.be.instanceof(Promise);
+
+      factory.destroy();
+    });
+
+    it('calls expected sub-methods', (done) => {
+      var factory;
+
+      factory = ASCE7_05Factory(_DUMMY_FACTORY);
+
+      sinon.spy(factory.metadataFactory, 'getMetadata');
+      sinon.spy(factory.probabilisticService, 'getData');
+
+      sinon.stub(factory, 'computeBasicDesign').callsFake(
+          () => {return Promise.resolve({}); });
+
+      sinon.spy(factory.siteAmplificationFactory, 'getSiteAmplificationData');
+      sinon.stub(factory, 'computeFinalDesign').callsFake(
+          () => { return Promise.resolve([]); });
+      sinon.spy(factory.designCategoryFactory, 'getDesignCategory');
+      sinon.stub(factory, 'computeSpectra').callsFake(
+          () => { return Promise.resolve([]); });
+      sinon.stub(factory, 'formatResult').callsFake(
+          () => { return Promise.resolve([]); });
+      sinon.stub(factory, 'makeMultipleRequests').callsFake(
+          () => { return Promise.resolve([]); });
+
+      factory.get({}).then((/*result*/) => {
+        expect(factory.metadataFactory.getMetadata.callCount).to.equal(1);
+        expect(factory.probabilisticService.getData.callCount).to.equal(1);
+
+        expect(factory.computeBasicDesign.callCount).to.equal(1);
+
+        expect(factory.siteAmplificationFactory
+            .getSiteAmplificationData.callCount).to.equal(1);
+        expect(factory.computeFinalDesign.callCount).to.equal(1);
+        expect(factory.designCategoryFactory
+            .getDesignCategory.callCount).to.equal(1);
+        expect(factory.computeSpectra.callCount).to.equal(1);
+
+        expect(factory.formatResult.callCount).to.equal(1);
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        factory.destroy();
+        done(err);
+      });
+    });
+  });
+});
