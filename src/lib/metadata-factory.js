91129c28b5b74b90c18330d116fa540d7ee89764
jdbrown-usgs <jdbrown@usgs.gov>
2017-09-14 11:32:42 -0600

    updated metadata and regions for ASCE7-05

--- src/lib/metadata-factory.js
+++ src/lib/metadata-factory.js
@@ -9,22 +9,18 @@ const _METADATA = {
   'ASCE7-05': [
     {
       'regions': [
-        'AK',
-        'AMSAM',
-        'COUS',
-        'GNMI',
-        'HI',
-        'PRVI'
+        'AK0P10',
+        'CANV0P01',
+        'CEUS0P01',
+        'HI0P02',
+        'PACNW0P01',
+        'PRVI0P05',
+        'SLC0P01',
+        'US0P05'
       ],
       'data': {
         'modelVersion': 'v2.0.x',
-        's1MaxDirFactor': 1,
-        's1dFloor': 0,
-        's1dPercentileFactor': 1,
-        'spatialInterpolationMethod': NumberUtils.INTERPOLATE_LINEARX_LINEARY_LINEAR,
-        'ssMaxDirFactor': 1,
-        'ssdFloor': 0,
-        'ssdPercentileFactor': 1
+        'spatialInterpolationMethod': NumberUtils.INTERPOLATE_LINEARX_LINEARY_LINEAR
       }
     }
   ],
@@ -214,11 +210,32 @@ const _REGIONS = [
     'min_longitude': -200.0
   },
   {
+    'name': 'AK0P10',
+    'max_latitude':    72.0,
+    'max_longitude': -130.0,
+    'min_latitude':    50.0,
+    'min_longitude': -190.0,
+  },
+  {
     'name': 'AMSAM',
     'max_latitude':   -11.0,
     'max_longitude': -165.0,
     'min_latitude':   -33.0,
     'min_longitude': -195.0
+  },
+  {
+    'name': 'CANV0P01',
+    'max_latitude':    42.0,
+    'max_longitude': -115.0,
+    'min_latitude':    32.0,
+    'min_longitude': -125.0,
+  },
+  {
+    'name': 'CEUS0P01',
+    'max_latitude':   38.0,
+    'max_longitude': -88.0,
+    'min_latitude':   35.0,
+    'min_longitude': -92.0,
   },
   {
     'name': 'COUS',
@@ -242,11 +259,46 @@ const _REGIONS = [
     'min_longitude': -161.0
   },
   {
+    'name': 'HI0P02',
+    'max_latitude':    23.0,
+    'max_longitude': -154.0,
+    'min_latitude':    18.0,
+    'min_longitude': -161.0
+  },
+  {
+    'name': 'PACNW0P01',
+    'max_latitude':    49.0,
+    'max_longitude': -123.0,
+    'min_latitude':    41.0,
+    'min_longitude': -125.0,
+  },
+  {
     'name': 'PRVI',
     'max_latitude':   19.0,
     'max_longitude': -64.5,
     'min_latitude':   17.5,
     'min_longitude': -67.5
+  },
+  {
+    'name': 'PRVI0P05',
+    'max_latitude':   21.0,
+    'max_longitude': -62.0,
+    'min_latitude':   16.0,
+    'min_longitude': -70.0,
+  },
+  {
+    'name': 'SLC0P01',
+    'max_latitude':    45.0,
+    'max_longitude': -110.0,
+    'min_latitude':    40.0,
+    'min_longitude': -112.0,
+  },
+  {
+    'name': 'US0P05',
+    'max_latitude':    50.0,
+    'max_longitude':  -65.0,
+    'min_latitude':    24.6,
+    'min_longitude': -125.0,
   }
 ];
 
