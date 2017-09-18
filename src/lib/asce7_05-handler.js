5d48d14b05a42cbe41092a0bbbb38353bea1fc1b
ehunter-usgs <ehunter@usgs.gov>
2017-09-18 17:36:38 -0600

    Update to work with refactored handler

--- src/lib/asce7_05-handler.js
+++ src/lib/asce7_05-handler.js
@@ -3,7 +3,8 @@
 
 const ASCE7_05Factory = require('./asce7_05-factory'),
     ASCE7Handler = require('./asce7-handler'),
-    extend = require('extend');
+    extend = require('extend'),
+    NumberUtils = require('./util/number-utils').instance;
 
 const _DEFAULTS = {
   factoryConstructor: ASCE7_05Factory,
@@ -18,6 +19,70 @@ const ASCE7_05Handler = function (options) {
   options = extend({}, _DEFAULTS, options);
   _this = ASCE7Handler(options);
 
+
+  /**
+   * Format the factory response
+   *
+   * @param result {Object}
+   *    An object containing all of the data from the ASCE7-05Factory
+   *
+   * @param {Promise<array>}
+   *    A promise resolving with an an object response values and metadata
+   *    values for the ASCE7-05 endpoint
+   */
+  _this.formatResult = function (result) {
+    return new Promise((resolve, reject) => {
+      let basicDesign,
+          finalDesign,
+          sdSpectrum,
+          siteAmplification,
+          smSpectrum,
+          spectra;
+
+      try {
+        basicDesign = result.basicDesign;
+        finalDesign = result.finalDesign;
+        siteAmplification = result.siteAmplification;
+        spectra = result.spectra;
+
+        if (siteAmplification.fa === null || siteAmplification.fv === null) {
+          sdSpectrum = null;
+          smSpectrum = null;
+        } else {
+          sdSpectrum = NumberUtils.roundSpectrum(spectra.sdSpectrum,
+              _this.outputDecimals);
+          smSpectrum = NumberUtils.roundSpectrum(spectra.smSpectrum,
+              _this.outputDecimals);
+        }
+
+        resolve({
+          data: {
+            ss: NumberUtils.round(basicDesign.ss, _this.outputDecimals),
+            fa: NumberUtils.round(siteAmplification.fa, _this.outputDecimals),
+            fa_error: siteAmplification.fa_error,
+            sms: NumberUtils.round(finalDesign.sms, _this.outputDecimals),
+            sds: NumberUtils.round(finalDesign.sds, _this.outputDecimals),
+
+            s1: NumberUtils.round(basicDesign.s1,
+                _this.outputDecimals),
+            fv: NumberUtils.round(siteAmplification.fv, _this.outputDecimals),
+            fv_error: siteAmplification.fv_error,
+            sm1: NumberUtils.round(finalDesign.sm1, _this.outputDecimals),
+            sd1: NumberUtils.round(finalDesign.sd1, _this.outputDecimals),
+
+            't-sub-l': result.tSubL,
+
+            sdSpectrum: sdSpectrum,
+            smSpectrum: smSpectrum
+          },
+
+          metadata: extend(true, {}, result.metadata)
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
 
   options = null;
   return _this;
--- test/asce7_05-handler.spec.js
+++ test/asce7_05-handler.spec.js
@@ -32,12 +32,108 @@ describe('asce7_05-handler', () => {
       factory = sinon.spy();
       referenceDocument = 'ASCE7-05';
       handler = ASCE7_05Handler({
-        factory: factory,
+        factoryConstructor: factory,
         referenceDocument: referenceDocument
       });
 
       expect(handler.referenceDocument).to.equal(referenceDocument);
       expect(factory.called).to.be.true;
+    });
+  });
+
+  describe('formatResult', () => {
+    it('returns a promise', () => {
+      let handler;
+
+      handler = ASCE7_05Handler();
+
+      expect(handler.formatResult()).to.be.instanceof(Promise);
+
+      handler.destroy();
+    });
+
+    it('resolves with expected data structure', (done) => {
+      let handler;
+
+      handler = ASCE7_05Handler();
+
+      handler.formatResult({
+        'basicDesign': {
+          'ss': null,
+          's1': null,
+          'pga': null,
+          'ssuh': null,
+          'ssrt': null,
+          'ssd': null,
+          's1uh': null,
+          's1rt': null,
+          's1d': null,
+          'pgad': null
+        },
+        'deterministic': {},
+        'finalDesign': {
+          'sms': null,
+          'sm1': null,
+          'pgam': null,
+          'sds': null,
+          'sd1': null
+        },
+        'metadata': {
+          'pgadPercentileFactor': null,
+          'pgadFloor': null,
+
+          's1MaxDirFactor': null,
+          's1dPercentileFactor': null,
+          's1dFloor': null,
+
+          'ssMaxDirFactor': null,
+          'ssdPercentileFactor': null,
+          'ssdFloor': null
+        },
+        'probabilistic': {},
+        'riskCoefficients': {
+          'response': {
+            'data': {}
+          }
+        },
+        'siteAmplification': {
+          'fa': null,
+          'fa_error': null,
+          'fv': null,
+          'fv_error': null
+        },
+        'designCategory': {
+          'sdcs': null,
+          'sdc1': null,
+          'sdc': null
+        },
+        'spectra': {
+          'smSpectrum': [],
+          'sdSpectrum': []
+        }
+      }).then((formatted) => {
+        [
+          'ss', 's1',
+          'sms', 'sm1',
+          'sds', 'sd1',
+          'fa', 'fa_error', 'fv', 'fv_error',
+          'smSpectrum', 'sdSpectrum'
+        ].forEach((key) => {
+          expect(formatted.data.hasOwnProperty(key)).to.equal(true);
+        });
+        [
+          'pgadPercentileFactor', 'pgadFloor',
+          's1MaxDirFactor', 's1dPercentileFactor', 's1dFloor',
+          'ssMaxDirFactor', 'ssdPercentileFactor', 'ssdFloor'
+        ].forEach((key) => {
+          expect(formatted.metadata.hasOwnProperty(key)).to.equal(true);
+        });
+      }).catch((err) => {
+        return err;
+      }).then((err) => {
+        handler.destroy();
+        done(err);
+      });
     });
   });
 });
