e4374655224476286bd701b5d9ba7abcef7fd407
jcaple <jcaple@usgs.gov>
2017-09-20 13:39:23 -0600

    Generating MarkDown reports for each QC test

--- update-quality-control.sh
+++ update-quality-control.sh
@@ -7,15 +7,29 @@
 
 pushd `dirname $0` > /dev/null 2>&1;
 
-outfile='./quality-control.md';
-
 echo "Running tests and sending output to ${outfile}.";
 echo "This may take several minutes to complete.";
 
-node \
+# The list contains the QC filename | Reference Document
+list=(
+    "asce7.qc.js|asce7-10"
+    "asce7.qc.js|asce7-16"
+    "asce7.qc.js|asce7-05"
+    "asce41.qc.js|asce41-13"
+)
+
+for i in "${!list[@]}";
+do
+  handler=`echo "${list[$i]}" | cut -d "|" -f 1`
+  switch=`echo "${list[$i]}" | cut -d "|" -f 2`
+
+  # Run the QC Reports individually indicating which report to run
+  # from the array specified above.
+  node \
   ./node_modules/mocha/bin/mocha \
-  test/qc/**/*.qc.js \
-  --reporter ../../../gh-reporter.js > $outfile;
+  test/qc/${handler} \
+  --reporter ../../../gh-reporter.js --${switch} > qc_reports/${switch}.md 2>&1
+done
 
 returnStatus=$?;
 
