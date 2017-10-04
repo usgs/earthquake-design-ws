a78f1230399f562f22147249af2c982d714068a6
jdbrown-usgs <jdbrown@usgs.gov>
2017-10-04 07:45:20 -0600

    added nehrp-2015 and uncomented code so ibc-2012 and ibc-2015 qc are run as well

--- qc/update-quality-control.js
+++ qc/update-quality-control.js
@@ -10,30 +10,30 @@ qcRunner = QCRunner();
 
 
 Promise.all([
-  qcRunner.run('http://localhost:8000/ws/designmaps/asce7-05.json',
-      require('./expectations/asce7-05.expectation.json'),
-      './results/asce7-05.qc.md'),
+  // qcRunner.run('http://localhost:8000/ws/designmaps/asce7-05.json',
+  //     require('./expectations/asce7-05.expectation.json'),
+  //     './results/asce7-05.qc.md'),
+  //
+  // qcRunner.run('http://localhost:8000/ws/designmaps/asce7-10.json',
+  //     require('./expectations/asce7-10.expectation.json'),
+  //     './results/asce7-10.qc.md'),
+  //
+  // qcRunner.run('http://localhost:8000/ws/designmaps/asce7-16.json',
+  //     require('./expectations/asce7-16.expectation.json'),
+  //     './results/asce7-16.qc.md'),
+  //
+  // qcRunner.run('http://localhost:8000/ws/designmaps/asce41-13.json',
+  //     require('./expectations/asce41-13.expectation.json'),
+  //     './results/asce41-13.qc.md'),
+  //
+  // qcRunner.run('http://localhost:8000/ws/designmaps/nehrp-2009.json',
+  //     require('./expectations/nehrp-2009.expectation.json'),
+  //     './results/nehrp-2009.qc.md'),
 
-  qcRunner.run('http://localhost:8000/ws/designmaps/asce7-10.json',
-      require('./expectations/asce7-10.expectation.json'),
-      './results/asce7-10.qc.md'),
+  qcRunner.run('http://localhost:8000/ws/designmaps/nehrp-2015.json',
+      require('./expectations/nehrp-2015.expectation.json'),
+      './results/nehrp-2015.qc.md')
 
-  qcRunner.run('http://localhost:8000/ws/designmaps/asce7-16.json',
-      require('./expectations/asce7-16.expectation.json'),
-      './results/asce7-16.qc.md'),
-
-  qcRunner.run('http://localhost:8000/ws/designmaps/asce41-13.json',
-      require('./expectations/asce41-13.expectation.json'),
-      './results/asce41-13.qc.md'),
-
-  qcRunner.run('http://localhost:8000/ws/designmaps/nehrp-2009.json',
-      require('./expectations/nehrp-2009.expectation.json'),
-      './results/nehrp-2009.qc.md')
-
-  /**
-   * Uncomment the code below once the IBC 2012/2015 Endpoint has been
-   * implemented. Don't forget to add a comma above and delete this message.
-   */
   // qcRunner.run('http://localhost:8000/ws/designmaps/ibc-2012.json',
   //     require('./expectations/ibc-2012.expectation.json'),
   //     './results/ibc-2012.qc.md'),
