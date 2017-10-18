9f2c8bb0d53aaef5091d8fc1f76e7f7b9ed8f9ee
jdbrown-usgs <jdbrown@usgs.gov>
2017-10-18 15:24:49 -0600

    data to be loaded into DB

---
+++ src/lib/db/metadata/regions.js
@@ -0,0 +1,98 @@
+'use strict';
+
+
+const _REGIONS = [
+  {
+    'name': 'AK0P05',
+    'max_latitude':    72.0,
+    'max_longitude': -125.1,
+    'min_latitude':    48.0,
+    'min_longitude': -200.0
+  },
+  {
+    'name': 'AK0P10',
+    'max_latitude':    72.0,
+    'max_longitude': -130.0,
+    'min_latitude':    50.0,
+    'min_longitude': -190.0,
+  },
+  {
+    'name': 'AMSAM0P10',
+    'max_latitude':   -11.0,
+    'max_longitude': -165.0,
+    'min_latitude':   -33.0,
+    'min_longitude': -195.0
+  },
+  {
+    'name': 'CANV0P01',
+    'max_latitude':    42.0,
+    'max_longitude': -115.0,
+    'min_latitude':    32.0,
+    'min_longitude': -125.0,
+  },
+  {
+    'name': 'CEUS0P01',
+    'max_latitude':   38.0,
+    'max_longitude': -88.0,
+    'min_latitude':   35.0,
+    'min_longitude': -92.0,
+  },
+  {
+    'name': 'COUS0P01',
+    'max_latitude':    50.0,
+    'max_longitude':  -65.0,
+    'min_latitude':    24.6,
+    'min_longitude': -125.0
+  },
+  {
+    'name': 'GNMI0P10',
+    'max_latitude':   23.0,
+    'max_longitude': 151.0,
+    'min_latitude':    9.0,
+    'min_longitude': 139.0
+  },
+  {
+    'name': 'HI0P02',
+    'max_latitude':    23.0,
+    'max_longitude': -154.0,
+    'min_latitude':    18.0,
+    'min_longitude': -161.0
+  },
+  {
+    'name': 'PACNW0P01',
+    'max_latitude':    49.0,
+    'max_longitude': -123.0,
+    'min_latitude':    41.0,
+    'min_longitude': -125.0,
+  },
+  {
+    'name': 'PRVI0P01',
+    'max_latitude':   19.0,
+    'max_longitude': -64.5,
+    'min_latitude':   17.5,
+    'min_longitude': -67.5
+  },
+  {
+    'name': 'PRVI0P05',
+    'max_latitude':   21.0,
+    'max_longitude': -62.0,
+    'min_latitude':   16.0,
+    'min_longitude': -70.0,
+  },
+  {
+    'name': 'SLC0P01',
+    'max_latitude':    45.0,
+    'max_longitude': -110.0,
+    'min_latitude':    40.0,
+    'min_longitude': -112.0,
+  },
+  {
+    'name': 'US0P05',
+    'max_latitude':    50.0,
+    'max_longitude':  -65.0,
+    'min_latitude':    24.6,
+    'min_longitude': -125.0,
+  }
+];
+
+module.exports = _REGIONS;
