c96240518f68fc6ef99b278a59339c96448af8fe
jdbrown-usgs <jdbrown@usgs.gov>
2017-10-18 15:25:12 -0600

    created DB schema

---
+++ src/lib/db/metadata/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE reference_document (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  value VARCHAR(255) NOT NULL,
+
+  UNIQUE (value)
+);
+
+CREATE TABLE region (
+  id SERIAL NOT NULL PRIMARY KEY,
+  reference_document_id INTEGER NOT NULL REFERENCES reference_document(id) ON DELETE CASCADE,
+
+  value VARCHAR(255) NOT NULL,
+  max_latitude NUMERIC NOT NULL,
+  max_longitude NUMERIC NOT NULL,
+  min_latitude NUMERIC NOT NULL,
+  min_longitude NUMERIC NOT NULL,
+
+
+  UNIQUE (reference_document_id, value)
+);
+
+CREATE TABLE reference_document_region_bridge (
+  id SERIAL NOT NULL PRIMARY KEY,
+
+  reference_document_id INTEGER NOT NULL REFERENCES reference_document(id) ON DELETE CASCADE,
+  region_id INTEGER NOT NULL REFERENCES region(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE metadata (
+  id SERIAL NOT NULL PRIMARY KEY,
+  reference_document_id INTEGER NOT NULL REFERENCES reference_document(id) ON DELETE CASCADE,
+  
+  key VARCHAR(255) NOT NULL,
+  value VARCHAR(255) NOT NULL
+);
