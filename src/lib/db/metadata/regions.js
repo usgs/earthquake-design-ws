2390cea3d5a34146b547786132f02d7b58025856
jdbrown-usgs <jdbrown@usgs.gov>
2017-10-19 13:25:14 -0600

    added grid spacing

--- src/lib/db/metadata/regions.js
+++ src/lib/db/metadata/regions.js
@@ -4,6 +4,7 @@
 const _REGIONS = [
   {
     'name': 'AK0P05',
+    'grid_spacing':    0.05,
     'max_latitude':    72.0,
     'max_longitude': -125.1,
     'min_latitude':    48.0,
@@ -11,6 +12,7 @@ const _REGIONS = [
   },
   {
     'name': 'AK0P10',
+    'grid_spacing':    0.10,
     'max_latitude':    72.0,
     'max_longitude': -130.0,
     'min_latitude':    50.0,
@@ -18,6 +20,7 @@ const _REGIONS = [
   },
   {
     'name': 'AMSAM0P10',
+    'grid_spacing':     0.1,
     'max_latitude':   -11.0,
     'max_longitude': -165.0,
     'min_latitude':   -33.0,
@@ -25,6 +28,7 @@ const _REGIONS = [
   },
   {
     'name': 'CANV0P01',
+    'grid_spacing':    0.01,
     'max_latitude':    42.0,
     'max_longitude': -115.0,
     'min_latitude':    32.0,
@@ -32,6 +36,7 @@ const _REGIONS = [
   },
   {
     'name': 'CEUS0P01',
+    'grid_spacing':   0.01,
     'max_latitude':   38.0,
     'max_longitude': -88.0,
     'min_latitude':   35.0,
@@ -39,6 +44,7 @@ const _REGIONS = [
   },
   {
     'name': 'COUS0P01',
+    'grid_spacing':    0.01,
     'max_latitude':    50.0,
     'max_longitude':  -65.0,
     'min_latitude':    24.6,
@@ -46,6 +52,7 @@ const _REGIONS = [
   },
   {
     'name': 'GNMI0P10',
+    'grid_spacing':    0.1,
     'max_latitude':   23.0,
     'max_longitude': 151.0,
     'min_latitude':    9.0,
@@ -53,6 +60,7 @@ const _REGIONS = [
   },
   {
     'name': 'HI0P02',
+    'grid_spacing':    0.02,
     'max_latitude':    23.0,
     'max_longitude': -154.0,
     'min_latitude':    18.0,
@@ -60,6 +68,7 @@ const _REGIONS = [
   },
   {
     'name': 'PACNW0P01',
+    'grid_spacing':    0.01,
     'max_latitude':    49.0,
     'max_longitude': -123.0,
     'min_latitude':    41.0,
@@ -67,6 +76,7 @@ const _REGIONS = [
   },
   {
     'name': 'PRVI0P01',
+    'grid_spacing':   0.01,
     'max_latitude':   19.0,
     'max_longitude': -64.5,
     'min_latitude':   17.5,
@@ -74,6 +84,7 @@ const _REGIONS = [
   },
   {
     'name': 'PRVI0P05',
+    'grid_spacing':   0.05,
     'max_latitude':   21.0,
     'max_longitude': -62.0,
     'min_latitude':   16.0,
@@ -81,6 +92,7 @@ const _REGIONS = [
   },
   {
     'name': 'SLC0P01',
+    'grid_spacing':    0.01,
     'max_latitude':    45.0,
     'max_longitude': -110.0,
     'min_latitude':    40.0,
@@ -88,6 +100,7 @@ const _REGIONS = [
   },
   {
     'name': 'US0P05',
+    'grid_spacing':    0.05,
     'max_latitude':    50.0,
     'max_longitude':  -65.0,
     'min_latitude':    24.6,
