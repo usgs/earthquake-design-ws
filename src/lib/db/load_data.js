ef3aef82a9088fa5ddf78569ff5a9e25c279b323
jdbrown-usgs <jdbrown@usgs.gov>
2017-10-19 13:29:57 -0600

    added metadata data loader

--- src/lib/db/load_data.js
+++ src/lib/db/load_data.js
@@ -5,6 +5,7 @@ const Config = require('../util/config'),
     dbUtils = require('./db-utils'),
     AbstractDataLoader = require('./abstract-data-loader'),
     DeterministicDataLoader = require('./deterministic/deterministic-data-loader'),
+    MetadataDataLoader = require('./metadata/metadata-data-loader'),
     ProbabilisticDataLoader = require('./probabilistic/probabilistic-data-loader'),
     RiskCoefficientDataLoader =
         require('./risk-coefficient/risk-coefficient-data-loader.js'),
@@ -17,6 +18,7 @@ const config = Config().get();
 
 const LOADER_FACTORIES = {
   'deterministic': DeterministicDataLoader,
+  'metadata': MetadataDataLoader,
   'probabilistic': ProbabilisticDataLoader,
   'risk-coefficient': RiskCoefficientDataLoader,
   'site-amplification': SiteAmplificationDataLoader,
