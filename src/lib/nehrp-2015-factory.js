6bbf42ee50036d7d025f94d2704c87de38ca8ae8
ehunter-usgs <ehunter@usgs.gov>
2017-11-06 16:23:26 -0700

    refactor asce7-16 and nehrp-2015 into nshmp-2014 factory

--- src/lib/nehrp-2015-factory.js
+++ src/lib/nehrp-2015-factory.js
@@ -1,8 +1,7 @@
 'use strict';
 
 
-const ASCE7Factory = require('./asce7-factory'),
-    VerticalCoefficientFactory = require('./vertical-coefficient-factory'),
+const NSHM2014Factory = require('./nshm_2014-factory'),
     extend = require('extend');
 
 
@@ -18,93 +17,13 @@ const _DEFAULTS = {
  *     Configuration options for this instance.
  */
 const NEHRP2015Factory = function (options) {
-  let _this,
-      _initialize,
-
-      _get;
+  let _this;
 
 
   options = extend({}, _DEFAULTS, options);
-  _this = ASCE7Factory(options);
-
-  _initialize = function(options) {
-    _this.verticalCoefficientFactory = options.verticalCoefficientFactory;
-    if (!_this.verticalCoefficientFactory) {
-      _this.destoryVerticalCoefficientFactory = true;
-      _this.verticalCoefficientFactory = VerticalCoefficientFactory();
-    }
-  };
+  _this = NSHM2014Factory(options);
 
 
-  _this.destroy = function() {
-    if (_this === null) {
-      return;
-    }
-
-    if (_this.destroyVerticalCoefficientFactory) {
-      _this.verticalCoefficientFactory.destroy();
-    }
-  };
-
-  _get = _this.get;
-  _this.get = function(inputs) {
-    let result;
-    return _get.call(_this, inputs).then((asce7Result) => {
-      result = asce7Result;
-      return _this.verticalCoefficientFactory.getVerticalCoefficientData({
-        ss:result.basicDesign.ss,
-        referenceDocument: _this.referenceDocument,
-        siteClass: inputs.siteClass
-      });
-    }).then((verticalCoefficientResult) => {
-      result.cv = verticalCoefficientResult.cv;
-      return _this.spectraFactory.getVerticalSpectrum(result.finalDesign.sms,
-          result.finalDesign.sm1, result.cv);
-    }).then((samvRawSpectrum) => {
-      let samvSpectrum,
-          savSpectrum;
-      samvSpectrum = [];
-      savSpectrum = [];
-
-      // SaMv = shall not be less than one-half of the corresponding SaM
-      // for horizontal components determined in accordance with the general or
-      // site-specific procedures of Section 11.4 or Chapter 21, respectively.
-      for (let i = 0; i < samvRawSpectrum.length; i++) {
-        let sam,
-            samv,
-            samvRaw,
-            sav;
-
-        // Samv calulation
-        sam = result.spectra.smSpectrum[i];
-        samvRaw = samvRawSpectrum[i];
-
-        if (sam[0] !== samvRaw[0]) {
-          throw new Error('Expected x values to match.');
-        }
-
-        samv = [
-          sam[0],
-          Math.max(samvRaw[1], 0.5 * sam[1])
-        ];
-
-        samvSpectrum.push(samv);
-
-        // Sav Calculation
-        sav = [samv[0], (2/3) * samv[1]];
-
-        savSpectrum.push(sav);
-      }
-
-      result.samvSpectrum = samvSpectrum;
-      result.savSpectrum = savSpectrum;
-
-      return result;
-    });
-  };
-
-
-  _initialize(options);
   options = null;
   return _this;
 };
--- test/nehrp-2015-factory.spec.js
+++ test/nehrp-2015-factory.spec.js
@@ -4,34 +4,7 @@
 
 const NEHRP2015Factory = require('../src/lib/nehrp-2015-factory'),
     expect = require('chai').expect;
-    //sinon = require('sinon');
 
-const _DUMMY_FACTORY = {
-  metadataService: {
-    getData: () => { return Promise.resolve({response: {data: {}}}); }
-  },
-  probabilisticService: {
-    getData: () => { return Promise.resolve({}); }
-  },
-  deterministicService: {
-    getData: () => { return Promise.resolve({}); }
-  },
-  riskCoefficientService: {
-    getData: () => { return Promise.resolve({}); }
-  },
-  tSubLService: {
-    getData: () => { return Promise.resolve({response: {data: {}}}); }
-  },
-  siteAmplificationFactory: {
-    getSiteAmplificationData: () => { return Promise.resolve([]); }
-  },
-  designCategoryFactory: {
-    getDesignCategory: () => { return Promise.resolve([]); }
-  },
-  spectraFactory: {
-    getSpectrum: () => { return Promise.resolve([]); }
-  }
-};
 
 describe('nehrp-2015-factory', () => {
   describe('constructor', () => {
@@ -54,23 +27,6 @@ describe('nehrp-2015-factory', () => {
 
       const factory = NEHRP2015Factory();
       expect(factory.referenceDocument).to.equal('NEHRP-2015');
-    });
-  });
-
-  describe('get', () => {
-    it('returns a promise and calls functions as intended', () => {
-
-      const factory = NEHRP2015Factory(_DUMMY_FACTORY);
-
-      let result = factory.get({
-        latitude: 0,
-        longitude: 1,
-        riskCategory: 2,
-        siteClass: 'C',
-        title: 'Test'
-      });
-
-      expect(result).to.be.instanceof(Promise);
     });
   });
 });
